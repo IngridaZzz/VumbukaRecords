5349cb7653111c3ae8c07e23b1efaf3860941c47
Emilie K. Lundsgaard <eaaemlu@students.eaaa.dk>
2019-06-21 15:44:38 +0200

    animation and contact-page
    contact-page and animations

--- js/script.js
+++ js/script.js
@@ -1,3 +1,62 @@
+//ANIMATION
+
+$(function() {
+//define variable $blocks as the classes implemented in HTML and $window as the window of the screen
+  var $blocks = $('.animBlock.notViewed');
+  var $window = $(window);
+
+//function(e) passes properties to the event handler 'scroll'. 
+    
+//adding event handler 'scroll'
+  $window.on('scroll', function(e){
+//Saying that when the blocks has the class 'viewed', the function isScrolledIntoView should start
+    $blocks.each(function(i,elem){
+      if($(this).hasClass('viewed')) 
+        return;
+        
+      isScrolledIntoView($(this));
+    });
+  });
+});
+
+function isScrolledIntoView(elem) {
+//defining the top of the screen as docViewTop
+  var docViewTop = $(window).scrollTop();
+//Defining the bottom of the screen as docViewBottom
+  var docViewBottom = docViewTop + $(window).height();
+//defining the elemOffset to 0.
+  var elemOffset = 0;
+
+//if the offset (coordinates) of the element are defined they resets to 0. The elements are as starting point not visible
+  if(elem.data('offset') != undefined) {
+    elemOffset = elem.data('offset');
+  }
+//elemTop defined as the top of the element(the blocks)
+  var elemTop = $(elem).offset().top;
+//elemBottom defined as the bottom of the element
+  var elemBottom = elemTop + $(elem).height();
+// custom offset is updated based on scrolling direction
+  if(elemOffset != 0) { 
+// scrolling up from bottom
+    if(docViewTop - elemTop >= 0) {
+      elemTop = $(elem).offset().top + elemOffset;
+    } else {
+// scrolling down from top
+      elemBottom = elemTop + $(elem).height() - elemOffset
+    }
+  }
+
+// once an element is visible exchange the classes
+//adding the class 'viewed' which is defined in CSS 
+  if((elemBottom <= docViewBottom) && (elemTop >= docViewTop)) {
+   $(elem).removeClass('notViewed').addClass('viewed');
+  }
+}
+
+
+
+
+
 // 	JQUERY COLLAPSE BOX WITH TRANSITION
 $(document).ready(function () {
     //action - when you click arrow1
